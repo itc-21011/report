b7862b3528571f76d80919383a0df53c377b5610
itc-21011 <s21011@std.it-college.ac.jp>
2023-02-28 17:53:39 +0900

    登録画面の表示ができた

--- app/Controllers/Http/SamplesController.ts
+++ app/Controllers/Http/SamplesController.ts
@@ -4,7 +4,7 @@ export default class SamplesController {
   public async index(ctx: HttpContextContract) {
     const data = {
       title: '出席管理',
-      message: 'ログイン'
+      message: 'ログインされていません。'
     }
     return ctx.view.render('reports/index', data)
   }
